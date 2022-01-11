7d44b4a12a0c7537eedee09d002254015b03a2ec
Heytemisan <temyicy@gmail.com>
2022-01-11 10:11:00 +0100

    second commit

--- ecommerce/src/components/Navbar/MenuItems.jsx
+++ ecommerce/src/components/Navbar/MenuItems.jsx
@@ -1,1 +1,28 @@
-const Menu
+export const Menu = [
+    { 
+        title: 'Shop',
+        url:'#',
+        cName:'nav-links'
+    },
+    { 
+        title: 'Pricelist',
+        url:'#',
+        cName:'nav-links'
+    },
+    { 
+        title: 'Benefit',
+        url:'#',
+        cName:'nav-links'
+    },
+    { 
+        title: 'Learn',
+        url:'#',
+        cName:'nav-links'
+    },
+    { 
+        text: 'Become a seller',
+        url:'#',
+        cName:'nav-links-button'
+    },
+    
+]
--- ecommerce/src/components/Navbar/Navbar.jsx
+++ ecommerce/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Menu} from './MenuItems';
+import './Navbar.css'
 
-export default class Navbar extends Component {
+class Navbar extends Component {
     render() {
         return (
-            <nav className='NavbarItems'>
+            <nav className='navbarItems'>
                 <h1 className="navbar-logo">SYDE</h1>
+                <div className="menu-icon">
+                    
+                </div>
                 <ul>
-                    <li><a href=""></a></li>
+                    {
+                        Menu.map((item, index) => { 
+                            return (
+                                <li key={index}>
+                                    <a className={item.cName} href={item.url}>
+                                    {item.title}
+                                    </a>
+                                </li>
+                            )
+                        })
+                    }
                 </ul>
             </nav>
         )
     }
 }
+
+export default Navbar;

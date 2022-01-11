d7461fb2d092bfa16b0d9feb008a13d675a6d70b
Heytemisan <temyicy@gmail.com>
2022-01-11 08:36:55 +0100

    first commit

---
+++ ecommerce/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <nav className='NavbarItems'>
+                <h1 className="navbar-logo">SYDE</h1>
+                <ul>
+                    <li><a href=""></a></li>
+                </ul>
+            </nav>
+        )
+    }
+}
